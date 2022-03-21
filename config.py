edaf57dc3532b166d33967aca6d72a547511e57d
AimError <dev@jildert.com>
2022-03-21 16:26:14 +0100

    Updated config.py
    changed default value for debug to False

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ STEERING_SIMILARITY_THRESHOLD = -1.0
 # help fine tune the steering threshold value. This value should not be 
 # lower then the value that is reported when the countersteering icon is red. 
 # Otherwise you'll be steering right the whole time and we do not want to do that in corners.
+# This will also give you a small window next to the chiaki stream this should display the 
+# countersteering icon.
 SHOW_SIMILARITY_DEBUG = False
